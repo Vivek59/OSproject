c9822e53a33d8914d2224ca9290a68176e71c4ae
Vivek59 <38816118+Vivek59@users.noreply.github.com>
2018-04-28 23:09:14 +0530

    Add files via upload

---
+++ os-project.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+char pn[10][10],t[10];
+int et[20],at[10],n,i,j,temp,p[10],st[10],ft[10],wt[10],ta[10];
+int totwt=0,totta=0;
+float awt,ata;
+printf("Please Enter the Process:");
+scanf("%d",&n);
+
